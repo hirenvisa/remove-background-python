685bb067c4a0ef6b42cb7cf71ddc1ffdc98511fd
hirenvisa <hiren.visavadiya+github@gmail.com>
2024-06-14 19:11:23 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, send_file, render_template,request
+from rembg import remove
+from PIL import Image
+from io import BytesIO
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return 'No file uploaded', 400
+        file = request.files['file']
+        if file.name == '':
+            return 'No file name is selected.', 400
+        if file:
+            input_image = Image.open(file.stream)
+            output_image = remove(input_image, post_process_mask = True)
+            img_io = BytesIO()
+            output_image.save(img_io, 'PNG')
+            img_io.seek(0)
+            return send_file(img_io, mimetype='image/png', as_attachment=True, download_name='rmbg.png')
+        
+
+
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True, port=5100)
